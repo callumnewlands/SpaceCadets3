73ae6aba698258041d0009e5636e826ee904ce6c
Callum Newlands <callumnewlands@googlemail.com>
2018-10-19 21:17:52 +0100

    Implemented copy to and swap methods

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -17,9 +17,12 @@ class Interpreter
     private Integer _linePtr = 0;
     private Stack<WhileLoopPtr> whileLoopPtrs = new Stack<>();
 
-    private static final String[] RESERVED_IDENTIFIERS = {"clear", "decr", "do", "end", "incr", "while"};
+    private static final String[] RESERVED_IDENTIFIERS = {"clear", "decr", "do", "end", "incr", "while", "swap", "copy", "to"};
     public static final String COMMENT_REG_EX = "\\s*#.*";
-    private static final String VARIABLE_REG_EX = "\\s*(clear|incr|decr)\\s+(\\w+)\\s*;\\s*";
+    private static final String UNARY_OPERATOR_REG_EX = "\\s*(clear|incr|decr)\\s+(\\w+)\\s*;\\s*";
+    private static final String BINARY_AFTER_OPERATOR_REG_EX = "\\s*(swap)\\s+(\\w+)\\s+(\\w+)\\s*;\\s*";
+    private static final String BINARY_MIDDLE_OPERATOR_REG_EX = "\\s*(\\w+)\\s+(\\+|-|/|\\*)\\s+(\\w+)\\s*;\\s*";
+    private static final String COPY_TO_REG_EX = "\\s*(copy)\\s+(\\w+)\\s+to\\s+(\\w+)\\s*;\\s*";
     public static final String KEYWORD_REG_EX = "\\b(" + String.join("|", RESERVED_IDENTIFIERS) + ")\\b";
     private static final String WHILE_REG_EX = "\\s*(while)\\s+(\\w+)\\s+not\\s+0\\s+do\\s*;\\s*";
     private static final String END_REG_EX = "\\s*(end)\\s*;\\s*";
@@ -62,18 +65,19 @@ class Interpreter
         if (!isSyntacticallyCorrect(line))
             throw new InterpreterException("Syntax Error in line " + (_linePtr + 1) + ": " + line);
 
-        Pattern variablePattern = Pattern.compile(VARIABLE_REG_EX);
+        Pattern variablePattern = Pattern.compile(UNARY_OPERATOR_REG_EX);
         Matcher variableMatcher = variablePattern.matcher(line);
         variableMatcher.find();
 
         // TODO: redo like GUI pattern matching with groups
 
         String instruction = "";
-        String variable = "";
+        String operator1 = "";
+        String operator2 = "";
         try
         {
             instruction = variableMatcher.group(1);
-            variable = variableMatcher.group(2);
+            operator1 = variableMatcher.group(2);
         }
         catch (IllegalStateException e)
         {
@@ -84,7 +88,7 @@ class Interpreter
             try
             {
                 instruction = whileMatcher.group(1);
-                variable = whileMatcher.group(2);
+                operator1 = whileMatcher.group(2);
             }
             catch (IllegalStateException e2)
             {
@@ -98,29 +102,78 @@ class Interpreter
                 }
                 catch (IllegalStateException e3)
                 {
-                    instruction = "";
-                    variable = "";
+
+                    Pattern binaryPattern = Pattern.compile(BINARY_AFTER_OPERATOR_REG_EX);
+                    Matcher binaryMatcher = binaryPattern.matcher(line);
+                    binaryMatcher.find();
+
+                    try
+                    {
+                        instruction = binaryMatcher.group(1);
+                        operator1 = binaryMatcher.group(2);
+                        operator2 = binaryMatcher.group(3);
+                    }
+                    catch (IllegalStateException e4)
+                    {
+
+                        Matcher binaryMiddleMatcher = Pattern.compile(BINARY_MIDDLE_OPERATOR_REG_EX).matcher(line);
+                        binaryMiddleMatcher.find();
+                        try
+                        {
+                            instruction = binaryMatcher.group(2);
+                            operator1 = binaryMatcher.group(1);
+                            operator2 = binaryMatcher.group(3);
+                        }
+                        catch (IllegalStateException e5)
+                        {
+
+                            Matcher copyToMatcher = Pattern.compile(COPY_TO_REG_EX).matcher(line);
+                            copyToMatcher.find();
+
+                            try
+                            {
+                                instruction = copyToMatcher.group(1);
+                                operator1 = copyToMatcher.group(2);
+                                operator2 = copyToMatcher.group(3);
+                            }
+                            catch (IllegalStateException e6)
+                            {
+                                operator1 = "";
+                                operator2 = "";
+                                instruction = "";
+                            }
+                        }
+
+                    }
+
                 }
             }
         }
 
-        if (isReservedIdentifier(variable))
-            throw new InterpreterException("Syntax Error in line " + (_linePtr + 1) + ": " + variable + " is a reserved keyword");
+        if (isReservedIdentifier(operator1))
+            throw new InterpreterException("Syntax Error in line " + (_linePtr + 1) + ": " + operator1 + " is a reserved keyword");
 
         switch (instruction)
         {
             case "clear":
-                clear(variable);
+                clear(operator1);
                 break;
             case "incr":
-                increment(variable);
+                increment(operator1);
                 break;
             case "decr":
-                decrement(variable);
+                decrement(operator1);
                 break;
+            case "swap":
+                swap(operator1, operator2);
+                break;
+            case "copy":
+                copyTo(operator1, operator2);
+                break;
+            //TODO: implement + - * / and change regex to take 3 args.
             case "while":
                 whileLoopPtrs.push(new WhileLoopPtr(_linePtr));
-                while (_variables.get(variable) > 0)
+                while (_variables.get(operator1) > 0)
                 {
                     _linePtr++;
                     execute();
@@ -165,6 +218,29 @@ class Interpreter
         }
     }
 
+    private void swap(String var1, String var2)
+    {
+        // TODO: extract method and replace with Map.putIfAbsent
+        if (_variables.get(var1) == null)
+            _variables.put(var1, 0);
+        if (_variables.get(var2) == null)
+            _variables.put(var2, 0);
+
+
+        Integer tmp = _variables.get(var1);
+        _variables.put(var1, _variables.get(var2));
+        _variables.put(var2, tmp);
+    }
+
+    private void copyTo(String var1, String var2)
+    {
+        // TODO: extract method and replace with Map.putIfAbsent
+        if (_variables.get(var1) == null)
+            _variables.put(var1, 0);
+
+        _variables.put(var2, _variables.get(var1));
+    }
+
     private void outputVariables()
     {
         _variables.forEach((variable, value) ->
@@ -194,17 +270,15 @@ class Interpreter
 
     private boolean isSyntacticallyCorrect(String line)
     {
-        Pattern variablePattern = Pattern.compile(VARIABLE_REG_EX);
-        Matcher variableMatcher = variablePattern.matcher(line);
+        Matcher unaryMatcher = Pattern.compile(UNARY_OPERATOR_REG_EX).matcher(line);
+        Matcher binaryAfterMatcher = Pattern.compile(BINARY_AFTER_OPERATOR_REG_EX).matcher(line);
+        Matcher whileMatcher = Pattern.compile(WHILE_REG_EX).matcher(line);
+        Matcher endMatcher = Pattern.compile(END_REG_EX).matcher(line);
+        Matcher binaryMiddleMatcher = Pattern.compile(BINARY_MIDDLE_OPERATOR_REG_EX).matcher(line);
+        Matcher copyToMatcher = Pattern.compile(COPY_TO_REG_EX).matcher(line);
 
-        Pattern whilePattern = Pattern.compile(WHILE_REG_EX);
-        Matcher whileMatcher = whilePattern.matcher(line);
-
-        Pattern endPattern = Pattern.compile(END_REG_EX);
-        Matcher endMatcher = endPattern.matcher(line);
-
-
-        return variableMatcher.matches() || whileMatcher.matches() || endMatcher.matches();
+        return unaryMatcher.matches() || binaryAfterMatcher.matches() || whileMatcher.matches()
+                || endMatcher.matches() || binaryMiddleMatcher.matches() || copyToMatcher.matches();
     }
 
     private boolean isReservedIdentifier(String id)
